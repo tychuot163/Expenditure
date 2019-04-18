f9a922b4db4b663d6f91a96e60c7d69b30de2fdf
Tusturu <tu.lqd.163@gmail.com>
2019-04-19 00:17:15 +0700

    Quản lý chi tiêu homework

---
+++ QLChiTieu/QLChiTieu.Tests/Controllers/ExpenditureControllerTest.cs
@@ -0,0 +1,56 @@
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using QLChiTieu.Models;
+using QLChiTieu.Controllers;
+namespace QLChiTieu.Tests.Controllers
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestIndex()
+        {
+            var db = new QLChiTieuEntities();
+            var controller = new ChiTieuController();
+            var result = controller.Index() as ViewResult;
+            Assert.IsNotNull(result);
+            var model = result.Model as List<Expenditure>;
+
+            Assert.AreEqual(db.Expenditures.Count(),
+                (model.Count));
+        }
+         [TestMethod]
+        public void TestEditG()
+        {
+            var controller = new ChiTieuController();
+            var result0 = controller.Edit(0);
+            Assert.IsInstanceOfType(result0, typeof(HttpNotFoundResult));
+
+            var db = new QLChiTieuEntities();
+            var item = db.Expenditures.First();
+            var result1 = controller.Edit(item.id) as ViewResult;
+            Assert.IsNotNull(result1);
+            var model = result1.Model as Expenditure;
+            Assert.AreEqual(item.id, model.id);
+
+        }
+         [TestMethod]
+         public void TestCreateG()
+         {
+
+             var controller = new ChiTieuController();
+
+             var result = controller.Create() as ViewResult;
+
+             Assert.IsNotNull(result);
+         }
+    }
+}
---
+++ QLChiTieu/QLChiTieu/Controllers/ChiTieuController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using QLChiTieu.Models;
+
+namespace QLChiTieu.Controllers
+{
+    public class ChiTieuController : Controller
+    {
+        private QLChiTieuEntities db = new QLChiTieuEntities();
+
+        // GET: /ChiTieu/
+        public ActionResult Index()
+        {
+            return View(db.Expenditures.ToList());
+        }
+
+        // GET: /ChiTieu/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Expenditure expenditure = db.Expenditures.Find(id);
+            if (expenditure == null)
+            {
+                return HttpNotFound();
+            }
+            return View(expenditure);
+        }
+
+        // GET: /ChiTieu/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /ChiTieu/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Expenditure model)
+        {
+            ValidateExpenditure(model);
+            if (ModelState.IsValid)
+            {
+                model.datetime = DateTime.Today;
+                db.Expenditures.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+        public void ValidateExpenditure(Expenditure model)
+        {
+            if (model.amount <= 0)
+            {
+                ModelState.AddModelError("amount", "Số tiền quá ít");
+            }
+        }
+
+        // GET: /ChiTieu/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            Expenditure expenditure = db.Expenditures.Find(id);
+            if (expenditure == null)
+            {
+                return HttpNotFound();
+            }
+            return View(expenditure);
+        }
+
+        // POST: /ChiTieu/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="amount,id,note,datetime")] Expenditure model)
+        {
+            ValidateExpenditure(model);
+            if (ModelState.IsValid)
+            {
+                db.Entry(model).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        // GET: /ChiTieu/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Expenditure expenditure = db.Expenditures.Find(id);
+            if (expenditure == null)
+            {
+                return HttpNotFound();
+            }
+            return View(expenditure);
+        }
+
+        // POST: /ChiTieu/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Expenditure expenditure = db.Expenditures.Find(id);
+            db.Expenditures.Remove(expenditure);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
